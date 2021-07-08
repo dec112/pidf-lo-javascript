e56c60111be3f4386d87a8b9f977df616330d618
Gabriel Unterholzer <unterholzer.gabriel@gmail.com>
2021-07-08 17:31:02 +0200

    Added new testcase

---
+++ test/wifi-location/index.test.ts
@@ -0,0 +1,26 @@
+import fs from 'fs';
+import path from 'path';
+
+import { Circle, PidfLo } from '../../dist/node';
+
+const loc = fs.readFileSync(path.join(__dirname, 'location.xml'), 'utf-8');
+
+describe('PidfLo Location Wifi GPS', () => {
+  it('parses xml', () => {
+    const parsed = PidfLo.fromXML(loc);
+
+    if (!parsed)
+      throw 'Could not parse location';
+
+    const { simple } = parsed;
+
+    expect(simple).toHaveProperty('latitude', 48.197457);
+    expect(simple).toHaveProperty('longitude', 14.482596);
+    expect(simple).toHaveProperty('radius', 270);
+    expect(simple).toHaveProperty('method', '');
+
+    expect(parsed.entity).toBe('sip:+43123456789@ims.mno.at');
+
+    expect(parsed.locationTypes[0].locations[0]).toBeInstanceOf(Circle);
+  });
+});
